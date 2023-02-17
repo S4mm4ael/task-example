2c7a892bd281a62ee4873e72dda226320b69a801
S4mm4el <homer1996@gmail.com>
2023-02-17 10:00:56 +0300

    feat: add ModalError component

--- src/components/modal-error/modal-error.tsx
+++ src/components/modal-error/modal-error.tsx
@@ -1,10 +1,15 @@
+import cross from '../../assets/svg/cross-black.svg'
+import warning from '../../assets/svg/warning-circle.svg'
 
 import styles from './modal-error.module.css';
 
 export function ModalError() {
   return (
     <div className={styles.ModalError} data-test-id='button-menu-view-list'>
-      Hello
+      <img src={warning} alt="warning" />
+      <p>Что-то пошло не так. Обновите страницу через некоторое время.</p>
+      <button className={styles.ModalError__close} type='button'> <img src={cross} alt="close" /></button>
+
     </div>
   );
 }
